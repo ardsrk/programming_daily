1795739229888bd530e78eb403f6bbe559aa01f5
Arvind <ardsrk@gmail.com>
2022-03-08 17:34:14 +0530

    Traveling salesman problem brute force

--- src/tsp_brute_force.rb
+++ src/tsp_brute_force.rb
@@ -14,5 +14,35 @@ G = {
   'a' => {'b' => 1, 'c' => 4, 'd' => 3},
   'b' => {'a' => 1, 'c' => 5, 'd' => 2},
   'c' => {'a' => 4, 'b' => 5, 'd' => 6},
-  'd' => {'a' => 3, 'b' => 2, 'd' => 6},
+  'd' => {'a' => 3, 'b' => 2, 'c' => 6},
 }
+
+# set of edges in tour
+t = {}
+
+# cost of tour
+cost = nil
+
+require_relative './permutation'
+
+permutation(V[1..-1]) do |vertices|
+  s = 'a'
+  tcost = 0
+  result = {}
+  (vertices + ['a']).each do |vertex|
+    (result[s] ||= {}).merge!({vertex => G[s][vertex]})
+    tcost += G[s][vertex]
+    s = vertex
+  end
+  if cost.nil? || tcost < cost
+    cost = tcost
+    t = result
+  end
+end
+
+require 'pp'
+puts "Input Graph:"
+pp G
+puts "Tour Cost (minimum): #{cost}"
+puts "Tour: "
+pp t
