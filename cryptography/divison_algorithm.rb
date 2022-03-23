aa0a7d7e6d3d15a0450ffc7d48572eca5cd3cfcd
Arvind <ardsrk@gmail.com>
2022-03-23 17:00:00 +0530

    division algorithm

---
+++ cryptography/divison_algorithm.rb
@@ -0,0 +1,35 @@
+# References:
+#   https://www.youtube.com/watch?v=DATIpbqTOG4&list=PL-qvsLbZq06LvdO6L7byZfcigeQAEo2k6&index=17
+
+def divide(dividend, divisor)
+  remainder = dividend
+  quotient = 0
+
+  if divisor <= 0
+    puts "Divisor must be a non-zero positive integer"
+    exit
+  end
+
+  if dividend >= 0
+    while remainder >= divisor
+      remainder = remainder - divisor
+      quotient = quotient + 1
+    end
+  else
+    while remainder < 0
+      remainder = remainder + divisor
+      quotient = quotient - 1
+    end
+  end
+  [quotient, remainder]
+end
+
+dividend = 45
+divisor = 11
+quotient, remainder = divide(dividend, divisor)
+puts "#{'%3d' % dividend} = #{'%2d' % quotient}*#{divisor} + #{remainder}"
+
+dividend = -45
+divisor = 11
+quotient, remainder = divide(dividend, divisor)
+puts "#{'%3d' % dividend} = #{'%2d' % quotient}*#{divisor} + #{remainder}"
