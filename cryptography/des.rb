45538490ed08856bcdb55832442bc944495d4dce
Arvind <ardsrk@gmail.com>
2022-07-17 23:34:29 +0530

    DES encryption

--- cryptography/des.rb
+++ cryptography/des.rb
@@ -76,6 +76,19 @@ class DES
     [63, 55, 47, 39, 31, 23, 15, 7],
   ]
 
+  # IP_INV: Used to permute 64-bit cipher-text obtained after 16 rounds
+  # of applying 16 sub-keys to plain-text
+  IP_INV = [
+    [40, 8, 48, 16, 56, 24, 64, 32],
+    [39, 7, 47, 15, 55, 23, 63, 31],
+    [38, 6, 46, 14, 54, 22, 62, 30],
+    [37, 5, 45, 13, 53, 21, 61, 29],
+    [36, 4, 44, 12, 52, 20, 60, 28],
+    [35, 3, 43, 11, 51, 19, 59, 27],
+    [34, 2, 42, 10, 50, 18, 58, 26],
+    [33, 1, 41,  9, 49, 17, 57, 25],
+  ]
+
   # S1-S8: S-boxes used in f function
   S1 = [
     [14,  4, 13, 1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9, 0,  7],
@@ -133,6 +146,30 @@ class DES
     [ 2,  1, 14, 7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11],
   ]
 
+  # Used by expansion function to expand 32-bit right cipher text to 48-bits
+  E = [
+    [32,  1,  2,  3,  4,  5],
+    [ 4,  5,  6,  7,  8,  9],
+    [ 8,  9, 10, 11, 12, 13],
+    [12, 13, 14, 15, 16, 17],
+    [16, 17, 18, 19, 20, 21],
+    [20, 21, 22, 23, 24, 25],
+    [24, 25, 26, 27, 28, 29],
+    [28, 29, 30, 31, 32,  1],
+  ]
+
+  # Used in the final step of f-function
+  P = [
+    [16,  7, 20, 21],
+    [29, 12, 28, 17],
+    [ 1, 15, 23, 26],
+    [ 5, 18, 31, 10],
+    [ 2,  8, 24, 14],
+    [32, 27,  3,  9],
+    [19, 13, 30,  6],
+    [22, 11,  4, 25],
+  ]
+
   # 56-bit key obtained by using PC_1 as lookup into BINARY_KEY
   def key_56
     PC_1.collect do |row|
@@ -151,6 +188,48 @@ class DES
 
   def d0
     @d0 ||= key_56[28..-1].to_i(2)
+  end
+
+  def l0
+    @l0 ||= ip.to_bin(DES::BLOCK_SIZE)[0..31].to_i(2)
+  end
+
+  def r0
+    @r0 ||= ip.to_bin(DES::BLOCK_SIZE)[32..-1].to_i(2)
+  end
+
+  # the expansion function
+  def e(r)
+    r_bin = r.to_bin(DES::BLOCK_SIZE/2)
+    E.collect do |row|
+      row.collect do |idx|
+        r_bin[idx-1]
+      end
+    end.flatten.join.to_i(2)
+  end
+
+  # the f function of DES
+  def f(r, k)
+    xored = e(r) ^ k
+    xored_bin = xored.to_bin(DES::SUB_KEY_SIZE)
+    bin_array = xored_bin.scan(/\d{6}/)
+    result = 1.upto(8).collect do |i|
+      send("s#{i}", bin_array[i-1])
+    end.join
+    P.collect do |row|
+      row.collect do |idx|
+        result[idx-1]
+      end
+    end.join.to_i(2)
+  end
+
+  1.upto(8) do |i|
+    define_method("s#{i}") do |b|
+      r_idx = "#{b[0]}#{b[-1]}".to_i(2)
+      c_idx = "#{b[1]}#{b[2]}#{b[3]}#{b[4]}".to_i(2)
+      val = self.class.const_get("S#{i}")[r_idx][c_idx]
+      val.to_bin(4)
+    end
   end
 
   1.upto(16) do |i|
@@ -192,6 +271,25 @@ class DES
         instance_variable_set("@k#{i}", result)
       end
     end
+
+    # define methods to generate 16 32-bit left and right cipher texts
+    define_method("l#{i}") do
+      result = instance_variable_get("@l#{i}")
+      if result
+        result
+      else
+        instance_variable_set("@l#{i}", send("r#{i-1}"))
+      end
+    end
+
+    define_method("r#{i}") do
+      result = instance_variable_get("@r#{i}")
+      if result
+        result
+      else
+        instance_variable_set("@r#{i}", send("l#{i-1}") ^ f(send("r#{i-1}"), send("k#{i}")))
+      end
+    end
   end
 
   def ip
@@ -204,10 +302,22 @@ class DES
     end.flatten.join.to_i(2)
   end
 
+  def ip_inv
+    return @ip_inv if @ip_inv
+    rl_bin = @rl_16.to_bin(BLOCK_SIZE)
+    @ip = IP_INV.collect do |row|
+      row.collect do |idx|
+        rl_bin[idx-1]
+      end
+    end.flatten.join.to_i(2)
+  end
+
   # m: 64-bit message
   def encrypt(m)
     @msg = m
     ip
+    @rl_16 = (send("r16").to_bin(DES::BLOCK_SIZE/2) + send("l16").to_bin(DES::BLOCK_SIZE/2)).to_i(2)
+    ip_inv.to_s(16)
   end
 end
 
@@ -218,26 +328,10 @@ end
 
 d = DES.new
 
-puts "         64-bit KEY: #{d.key}"
-puts
+puts "       KEY: #{d.key}"
 
 # 64-bit message
 m = "0123456789ABCDEF"
 
-puts "56-bit permuted KEY: #{d.key_56.to_i(2).to_s(16).upcase}"
-puts
-
-0.upto(16) do |i|
-  puts "#{'%3s: ' % ("c"+i.to_s) }" + d.send("c#{i}").to_bin(DES::KEY_SIZE/2)
-  puts "#{'%3s: ' % ("d"+i.to_s) }" + d.send("d#{i}").to_bin(DES::KEY_SIZE/2)
-  puts
-end
-
-1.upto(16) do |i|
-  puts "#{'%3s: ' % ("k"+i.to_s)}" + pretty_print(d.send("k#{i}").to_bin(DES::SUB_KEY_SIZE), 6)
-  puts
-end
-
-puts " M: #{pretty_print(m.to_i(16).to_bin(DES::BLOCK_SIZE), 4)}"
-d.encrypt(m)
-puts "IP: #{pretty_print(d.ip.to_bin(DES::BLOCK_SIZE), 4)}"
+puts " PlainText: #{m}"
+puts "CipherText: #{d.encrypt(m).upcase}"
